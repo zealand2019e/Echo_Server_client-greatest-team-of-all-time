b76874b39024cf79f53b5898ea5a418bca0211df
jhnzrep <jcoyne410@gmail.com>
2019-09-10 20:46:55 +0200

    x

---
+++ EchoChat/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+            while(true)
+            {
+                Task.Run(() =>
+                {
+                    TcpClient tempSocket = serverSocket.AcceptTcpClient();
+                    DoClient(tempSocket);
+                });
+            }
+            //Console.WriteLine("server started witing for connection!");
+
+            //TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            ////Socket connectionSocket = serverSocket.AcceptSocket();
+            //Console.WriteLine("Server activated");
+
+            //DoClient(connectionSocket);
+
+            //connectionSocket.Close();
+            //serverSocket.Stop();
+        }
+        
+        public static void DoClient(TcpClient socket)
+        { 
+
+            Stream ns = socket.GetStream();
+            // Stream ns = new NetworkStream(connectionSocket);
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                int first, second;
+                var splitstring = message.Split(' ');
+                first = Int32.Parse(splitstring[0]);
+                second = Int32.Parse(splitstring[1]);
+                answer = Convert.ToString(first + second);
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+            ns.Close();
+        }
+    }
+}
+
---
+++ EchoClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World it is client!");
+            Console.ReadLine();
+            TcpClient clientSocket = new TcpClient("localhost", 6789);
+            Stream ns = clientSocket.GetStream();
+            while (true)
+            {
+                 //provides a Stream
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                string serverAnswer = sr.ReadLine();
+                Console.WriteLine("Server: " + serverAnswer);
+            }
+
+            ns.Close();
+
+            clientSocket.Close();
+        }
+    }
+}
